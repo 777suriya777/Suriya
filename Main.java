323bf79d9c6a066fe157fd5ac6cadee8514f5c7a
SuriyaPrasath Balamurugan <136943127+777suriya777@users.noreply.github.com>
2024-01-31 15:42:50 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2232,6 +2232,18 @@ public class Main {
         }
         System.out.print(maxSum);
     }
+    public static void mountain(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int len = sc.nextInt();
+        int[] arr = new int[len];
+        for(int i=0;i<len;i++) arr[i] = sc.nextInt();
+        for(int i=0;i<len;i++){
+            if(arr[i]>arr[i+1]){
+                System.out.print(i);
+                break;
+            }
+        }
+    }
     //------------------------- S4 ---------------------------------------------
      public static void secondFrequentCharacter(String[] args){
         Scanner sc = new Scanner(System.in);
