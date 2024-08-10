807cbdfb8a0173e2c8febcedd50f11dcd4ffbd27
SuriyaPrasath Balamurugan <136943127+777suriya777@users.noreply.github.com>
2024-08-10 09:28:20 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3206,4 +3206,65 @@ public static void rotateMatrixElements(String[] args){
             n--;
         }
     }
+static ArrayList<String>
+    letterCombinationsUtil(int[] number, int n,
+                           String[] table)
+    {
+        // To store the generated letter combinations
+        ArrayList<String> list = new ArrayList<>();
+ 
+        Queue<String> q = new LinkedList<>();
+        q.add("");
+ 
+        while (!q.isEmpty()) {
+            String s = q.remove();
+ 
+            // If complete word is generated
+            // push it in the list
+            if (s.length() == n)
+                list.add(s);
+            else {
+                String val = table[number[s.length()]];
+                for (int i = 0; i < val.length(); i++) 
+                {
+                    q.add(s + val.charAt(i));
+                }
+            }
+        }
+        return list;
+    }
+ 
+    // Function that creates the mapping and
+    // calls letterCombinationsUtil
+    static void letterCombinations(int[] number, int n)
+    {
+        // table[i] stores all characters that
+        // corresponds to ith digit in phone
+        String[] table
+            = { "0",   "1",   "abc",  "def", "ghi",
+                "jkl", "mno", "pqrs", "tuv", "wxyz" };
+ 
+        ArrayList<String> list
+            = letterCombinationsUtil(number, n, table);
+ 
+        // Print the contents of the list
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+        }
+    }
+ 
+    // Driver code
+    public static void letterC(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        String[] s = sc.nextLine().split("");
+        if(s[0].equals("1")){
+            System.out.println("No Combination of Strings");
+            return;
+        }
+        int number[] = new int[s.length];
+        for(int i=0;i<s.length;i++) number[i] = Integer.parseInt(s[i]);
+        int n = number.length;
+        letterCombinations(number, n);
+    }
 }
