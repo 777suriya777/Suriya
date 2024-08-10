6c61dccec47c7daeb084ad3ce06f490644f9288c
SuriyaPrasath Balamurugan <136943127+777suriya777@users.noreply.github.com>
2024-08-10 09:35:52 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -3267,4 +3267,94 @@ static ArrayList<String>
         int n = number.length;
         letterCombinations(number, n);
     }
+static void counterClockspiralPrint(int m, 
+                                        int n, 
+                                        int arr[][])
+    {
+        int i, k = 0, l = 0;
+ 
+    /* k - starting row index
+        m - ending row index
+        l - starting column index
+        n - ending column index
+        i - iterator */
+ 
+        // initialize the count
+        int cnt = 0;
+ 
+        // total number of
+        // elements in matrix
+        int total = m * n;
+ 
+        while (k < m && l < n) 
+        {
+            if (cnt == total)
+                break;
+ 
+            // Print the first column 
+            // from the remaining columns
+            for (i = k; i < m; ++i)
+            {
+                System.out.print(arr[i][l] + " ");
+                cnt++;
+            }
+            l++;
+ 
+            if (cnt == total)
+                break;
+ 
+            // Print the last row from
+            // the remaining rows 
+            for (i = l; i < n; ++i) 
+            {
+                System.out.print(arr[m - 1][i] + " ");
+                cnt++;
+            }
+            m--;
+ 
+            if (cnt == total)
+                break;
+ 
+            // Print the last column 
+            // from the remaining columns 
+            if (k < m) 
+            {
+                for (i = m - 1; i >= k; --i) 
+                {
+                    System.out.print(arr[i][n - 1] + " ");
+                    cnt++;
+                }
+                n--;
+            }
+ 
+            if (cnt == total)
+                break;
+ 
+            // Print the first row 
+            // from the remaining rows
+            if (l < n) 
+            {
+                for (i = n - 1; i >= l; --i) 
+                {
+                    System.out.print(arr[k][i] + " ");
+                    cnt++;
+                }
+                k++;
+            }
+        }
+    }
+ 
+// Driver Code
+public static void antiClockwiseSpiral(String[] args)
+{
+    Scanner sc = new Scanner(System.in);
+    int R = sc.nextInt();
+    int C = sc.nextInt();
+    int arr[][] = new int[R][C];
+     for(int i=0;i<R;i++){
+         for(int j=0;j<C;j++) arr[i][j] = sc.nextInt();
+     }
+    // Function calling             
+    counterClockspiralPrint(R, C, arr);
+}
 }
